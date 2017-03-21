ea9d8c7c9924c20acfc56e6529192c789087ce62
Bereket Ghebremedhin <Bear@Stefanoss-MacBook-Pro.local>
2017-03-20 20:59:23 -0400

    first commit, did some UI stuff

---
+++ CloudyNomad/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CloudyNomad
+//
+//  Created by Bereket Ghebremedhin on 3/20/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var gradientImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        displayBackground()
+    }
+    
+    func displayBackground(){
+        let randomNum = arc4random_uniform(UInt32(5)) + UInt32(1)
+        let index = Int(randomNum)
+        gradientImageView.image = UIImage(named:"gradient\(index).jpeg")
+    }
+    
+    
+}
+
