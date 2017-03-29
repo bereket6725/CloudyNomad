167c072e71054a322cb8379f842284b3622977eb
Bereket Ghebremedhin <bereket6725@gmail.com>
2017-03-28 20:21:58 -0400

    rearranged the order of code in the RequestBuilder file.

--- CloudyNomad/RequestBuilder.swift
+++ CloudyNomad/RequestBuilder.swift
@@ -16,6 +16,35 @@ enum Result <T, E:Error>{
     case success(T)
     case failure(E)
 }
+
+enum RequestType{
+    case get
+    case post([String: Any])
+    
+    var methodType: Alamofire.HTTPMethod {
+        switch self {
+        case .get:
+            return .get
+        case .post:
+            return .post
+        }
+    }
+}
+
+struct ApiKeys{
+    fileprivate static let openWeatherMaps = "26db8fa456dbb4ac68876d86cbaefc22"
+    fileprivate static let googleTimeZone = "AIzaSyAlTJ6_w_Pfy1nRatUt4qV1uj8436hMxEk"
+    fileprivate static let Meetup = "4a465f213f5d8047321b627874238070"
+
+}
+//Our Request buildables will have the baseURLs and will create the URL that will eventually be sent in the request.
+//So Im making these fileprivate because I dont see anyone else needing this information
+struct BaseURLStrings{
+    fileprivate static let openWeatherMaps = "api.openweathermap.org/data/2.5/weather?"
+    fileprivate static let googleTimeZone = "“https://maps.googleapis.com/maps/api/timezone/json?location="
+    fileprivate static let Meetup = ""
+}
+
 //A-C will be conforming to the RequestBuildable and hold the information necessary to make network requests
 //A
 struct OpenWeatherMapsRequest: RequestBuildable{
@@ -49,31 +78,3 @@ struct GoogleTimeZoneRequest{
 //C
 struct MeetupRequest{
 }
-enum RequestType{
-    case get
-    case post([String: Any])
-    
-    var methodType: Alamofire.HTTPMethod {
-        switch self {
-        case .get:
-            return .get
-        case .post:
-            return .post
-        }
-    }
-}
-
-struct ApiKeys{
-    fileprivate static let openWeatherMaps = "26db8fa456dbb4ac68876d86cbaefc22"
-    fileprivate static let googleTimeZone = "AIzaSyAlTJ6_w_Pfy1nRatUt4qV1uj8436hMxEk"
-    fileprivate static let Meetup = "4a465f213f5d8047321b627874238070"
-
-}
-//Our Request buildables will have the baseURLs and will create the URL that will eventually be sent in the request.
-//So Im making these fileprivate because I dont see anyone else needing this information
-struct BaseURLStrings{
-    fileprivate static let openWeatherMaps = "api.openweathermap.org/data/2.5/weather?"
-    fileprivate static let googleTimeZone = "“https://maps.googleapis.com/maps/api/timezone/json?location="
-    fileprivate static let Meetup = ""
-}
-
---
+++ CloudyNomad/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  CloudyNomad
+//
+//  Created by Bereket Ghebremedhin on 3/28/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import Foundation
